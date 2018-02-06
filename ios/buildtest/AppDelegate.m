ebc36fa006a133d7dd568ce1eef2af9d2f407fa0
Will Prater <will@superformula.com>
2018-02-06 15:25:03 -0800

    add bugsnag react native

--- ios/buildtest/AppDelegate.m
+++ ios/buildtest/AppDelegate.m
@@ -12,10 +12,14 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import <BugsnagReactNative/BugsnagReactNative.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [BugsnagReactNative start];
+
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
