71997e912c8b6d879cff11d9a9bcc204412e969f
Will Prater <will@superformula.com>
2018-02-07 18:57:57 -0800

    echo

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-git symbolic-ref --short -q HEAD
+echo git symbolic-ref --short -q HEAD
 echo $?
 
-git symbolic-ref HEAD 2>/dev/null | cut -d"/" -f 3
+echo git symbolic-ref HEAD 2>/dev/null | cut -d"/" -f 3
 echo $?
